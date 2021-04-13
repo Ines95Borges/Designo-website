9f023723d0f5c663812f1ef9edda357cfe228b5e
Inês Barata Feio Borges <inesbarataborges@gmail.com>
2021-04-13 16:16:04 +0100

    finalized about us and locations page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import ContactLink from '../components/ContactLink';
+import Footer from '../components/Footer';
 
 const About = () => {
   return(
@@ -21,6 +23,66 @@ const About = () => {
       <div className="bg-rounded-rect-about">
         <svg width="1006" height="594" xmlns="http://www.w3.org/2000/svg"><path d="M297 0h412c164.029 0 297 132.971 297 297S873.029 594 709 594H0V297C0 132.971 132.971 0 297 0z" fill="#F1F3F5" fill-rule="evenodd" opacity=".502"/></svg>
       </div>
+
+      <div className="left-image">
+        <img src="/assets/about/desktop/image-world-class-talent.jpg" alt="World class talent"/>
+        <div className="left-image__content">
+          <h2 className="left-image__title">World-class talent</h2>
+          <p className="left-image__text1">We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully crafted from concept to launch, ensuring success in its given market. We are constantly updating our skills in a myriad of platforms.</p>
+          <p className="left-image__text2">Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality outcomes that encapsulates their brand’s story and mission.</p>
+        </div>
+      </div>
+
+      <div className="locations-about">
+        <div className="locations-about__canada">
+          <div className="locations-about__images">
+            <div className="locations-about__images__small-circle1">
+              <svg xmlns="http://www.w3.org/2000/svg" width="202" height="202"><defs><linearGradient id="a" x1="0%" x2="100%" y1="50%" y2="50%"><stop offset="0%" stop-color="#5D0202" stop-opacity="0"/><stop offset="100%" stop-color="#5D0202" stop-opacity=".498"/></linearGradient></defs><circle cx="101" cy="101" r="101" fill="url(#a)" fill-rule="evenodd" opacity=".203" transform="matrix(-1 0 0 1 202 0)"/></svg>
+            </div>
+            <img src="/assets/shared/desktop/illustration-canada.svg" alt="Canada"/>
+          </div>
+          <h3 className="locations-about__title">Canada</h3>
+          <button className="locations-about--btn">See location</button>
+        </div>
+
+        <div className="locations-about__australia">
+          <div className="locations-about__images">
+            <div className="locations-about__images__small-circle2">
+              <svg xmlns="http://www.w3.org/2000/svg" width="202" height="202"><defs><linearGradient id="a" x1="0%" x2="100%" y1="50%" y2="50%"><stop offset="0%" stop-color="#5D0202" stop-opacity="0"/><stop offset="100%" stop-color="#5D0202" stop-opacity=".498"/></linearGradient></defs><circle cx="101" cy="101" r="101" fill="url(#a)" fill-rule="evenodd" opacity=".203" transform="matrix(-1 0 0 1 202 0)"/></svg>
+            </div>
+            <img src="/assets/shared/desktop/illustration-australia.svg" alt="Australia"/>
+          </div>
+          <h3 className="locations-about__title">Australia</h3>
+          <button className="locations-about--btn">See location</button>
+        </div>
+
+        <div className="locations-about__uk">
+          <div className="locations-about__images">
+            <div className="locations-about__images__small-circle3">
+              <svg xmlns="http://www.w3.org/2000/svg" width="202" height="202"><defs><linearGradient id="a" x1="0%" x2="100%" y1="50%" y2="50%"><stop offset="0%" stop-color="#5D0202" stop-opacity="0"/><stop offset="100%" stop-color="#5D0202" stop-opacity=".498"/></linearGradient></defs><circle cx="101" cy="101" r="101" fill="url(#a)" fill-rule="evenodd" opacity=".203" transform="matrix(-1 0 0 1 202 0)"/></svg>
+            </div>
+            <img src="/assets/shared/desktop/illustration-united-kingdom.svg" alt="United kingdom"/>
+          </div>
+          <h3 className="locations-about__title">United Kingdom</h3>
+          <button className="locations-about--btn">See location</button>
+        </div>
+      </div>
+
+      <div className="right-image">
+        <div className="right-image__content">
+          <h2 className="right-image__title">The real deal</h2>
+          <p className="right-image__text1">As strategic partners in our clients’ businesses, we are ready to take on any challenge as our own. Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every opportunity. We make design and technology more accessible and give you tools to measure success.</p>
+          <p className="right-image__text2">We are visual storytellers in appealing and captivating ways. By combining business and marketing strategies, we inspire audiences to take action and drive real results.</p>
+        </div>
+        <img src="/assets/about/desktop/image-real-deal.jpg" alt="Real deal"/>
+      </div>
+
+      <div className="about-bg-image-rounded-rect">
+        <svg width="1006" height="594" xmlns="http://www.w3.org/2000/svg"><path d="M297 0h412c164.029 0 297 132.971 297 297S873.029 594 709 594H0V297C0 132.971 132.971 0 297 0z" fill="#F1F3F5" fill-rule="evenodd" opacity=".502"/></svg>
+      </div>
+
+      <ContactLink />
+      <Footer />
     </>
   )
 }
--- src/pages/Locations.js
+++ src/pages/Locations.js
@@ -1,8 +1,101 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
+import ContactLink from '../components/ContactLink';
+import Footer from '../components/Footer';
 
 const Locations = () => {
   return(
-    <h1>Locations</h1>
+    <>
+      <Navbar />
+      <section className="locations">
+        <div className="locations__canada">
+          <div className="locations__description">
+            <div className="locations__small-circles">
+              <div className="locations__small-circle1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="202" height="202"><defs><linearGradient id="a" x1="0%" x2="100%" y1="50%" y2="50%"><stop offset="0%" stop-color="#5D0202" stop-opacity="0"/><stop offset="100%" stop-color="#5D0202" stop-opacity=".498"/></linearGradient></defs><circle cx="101" cy="101" r="101" fill="url(#a)" fill-rule="evenodd" opacity=".203" transform="matrix(-1 0 0 1 202 0)"/></svg>
+              </div>
+              <div className="locations__small-circle2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="202" height="202"><defs><linearGradient id="a" x1="0%" x2="100%" y1="50%" y2="50%"><stop offset="0%" stop-color="#5D0202" stop-opacity="0"/><stop offset="100%" stop-color="#5D0202" stop-opacity=".498"/></linearGradient></defs><circle cx="101" cy="101" r="101" fill="url(#a)" fill-rule="evenodd" opacity=".203" transform="matrix(-1 0 0 1 202 0)"/></svg>
+              </div>
+            </div>
+            <div className="locations__content">
+              <h2 className="locations__title">Canada</h2>
+              <div></div>
+              <address>
+                <strong>Designo Central Office</strong><br/>
+                3886 Wellington Street<br/>
+                Toronto, Ontario M9C 3J5
+              </address>
+              <address>
+                <strong>Contact</strong><br/> 
+                P : +1 253-863-8967<br/>
+                M : contact@designo.co
+              </address>
+            </div>
+          </div>
+          <img src="/assets/locations/desktop/image-map-canada.png" alt="Canada location"/>
+        </div>
+
+        <div className="locations__australia">
+          <img src="/assets/locations/desktop/image-map-australia.png" alt="Australia location"/>
+          <div className="locations__description">
+            <div className="locations__small-circles">
+              <div className="locations__small-circle1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="202" height="202"><defs><linearGradient id="a" x1="0%" x2="100%" y1="50%" y2="50%"><stop offset="0%" stop-color="#5D0202" stop-opacity="0"/><stop offset="100%" stop-color="#5D0202" stop-opacity=".498"/></linearGradient></defs><circle cx="101" cy="101" r="101" fill="url(#a)" fill-rule="evenodd" opacity=".203" transform="matrix(-1 0 0 1 202 0)"/></svg>
+              </div>
+              <div className="locations__small-circle2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="202" height="202"><defs><linearGradient id="a" x1="0%" x2="100%" y1="50%" y2="50%"><stop offset="0%" stop-color="#5D0202" stop-opacity="0"/><stop offset="100%" stop-color="#5D0202" stop-opacity=".498"/></linearGradient></defs><circle cx="101" cy="101" r="101" fill="url(#a)" fill-rule="evenodd" opacity=".203" transform="matrix(-1 0 0 1 202 0)"/></svg>
+              </div>
+            </div>
+            <div className="locations__content">
+              <h2 className="locations__title">Australia</h2>
+              <div></div>
+              <address>
+                <strong>Designo AU Office</strong><br/>
+                19 Balonne Street<br/>
+                New South Wales 2443
+              </address>
+              <address>
+                <strong>Contact</strong><br/> 
+                P : (02) 6720 9092<br/>
+                M : contact@designo.au
+              </address>
+            </div>
+          </div>
+        </div>
+
+        <div className="locations__uk">
+          <div className="locations__description">
+            <div className="locations__small-circles">
+              <div className="locations__small-circle1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="202" height="202"><defs><linearGradient id="a" x1="0%" x2="100%" y1="50%" y2="50%"><stop offset="0%" stop-color="#5D0202" stop-opacity="0"/><stop offset="100%" stop-color="#5D0202" stop-opacity=".498"/></linearGradient></defs><circle cx="101" cy="101" r="101" fill="url(#a)" fill-rule="evenodd" opacity=".203" transform="matrix(-1 0 0 1 202 0)"/></svg>
+              </div>
+              <div className="locations__small-circle2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="202" height="202"><defs><linearGradient id="a" x1="0%" x2="100%" y1="50%" y2="50%"><stop offset="0%" stop-color="#5D0202" stop-opacity="0"/><stop offset="100%" stop-color="#5D0202" stop-opacity=".498"/></linearGradient></defs><circle cx="101" cy="101" r="101" fill="url(#a)" fill-rule="evenodd" opacity=".203" transform="matrix(-1 0 0 1 202 0)"/></svg>
+              </div>
+            </div>
+            <div className="locations__content">
+              <h2 className="locations__title">United Kingdom</h2>
+              <div></div>
+              <address>
+                <strong>Designo UK Office</strong><br/>
+                13  Colorado Way<br/>
+                Rhyd-y-fro SA8 9GA
+              </address>
+              <address>
+                <strong>Contact</strong><br/> 
+                P : 078 3115 1400<br/>
+                M : contact@designo.uk
+              </address>
+            </div>
+          </div>
+            <img src="/assets/locations/desktop/image-map-united-kingdom.png" alt="UK location"/>
+        </div>
+      </section>
+
+      <ContactLink />
+      <Footer />
+    </>
   )
 }
 
